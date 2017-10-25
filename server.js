c9cf0d53a83b07e8bc7dd815c61ac6d50e53c8ff
jailbreak26 <jailbreak31@gmail.com>
2017-10-25 15:13:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.use(bodyParser.json({limit: '3mb'}));
 app.use('/static/dist', express.static(__dirname + '/dist'));
 app.use('/static/semantic', express.static(__dirname + '/public/semantic'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/templates/index.html');
 });
